4362291c6057a31bc416934bb5cc452eddbc9d25
dariorumi5 <dariorumi5@alumnos.ilerna.com>
2026-03-05 13:53:51 +0100

    proyectil hecho

--- khaenriah/src/main/java/com/ilerna/modelos/Proyectil.java
+++ khaenriah/src/main/java/com/ilerna/modelos/Proyectil.java
@@ -1,5 +1,26 @@
 package com.ilerna.modelos;
 
-public class Proyectil {
+import java.awt.Rectangle;
 
+public class Proyectil {
+    public double x, y;
+    public int velocidad, ancho, alto;
+
+    public Proyectil(double x, double y, int velocidad, int ancho, int alto) {
+        this.x = x;
+        this.y = y;
+        this.velocidad = velocidad;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    // Para cuando el proyectil
+    public Rectangle getBounds() {
+        return new Rectangle((int) x, (int) y, ancho, alto);
+    }
+
+    // Para mover el proyectil hacia arriba.
+    public void mover() {
+        y -= velocidad;
+    }
 }
