6812e2b975be366bacaf889bac36a1fa342ad7fd
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-03-05 17:39:16 +0100

    JPanel1 arreglado

--- khaenriah/src/main/java/com/ilerna/vista/JPanel1Menu.java
+++ khaenriah/src/main/java/com/ilerna/vista/JPanel1Menu.java
@@ -6,14 +6,18 @@ import javax.swing.*;
 
 public class JPanel1Menu extends JPanel {
 
+    private Image imagenFondo;
+
     public JPanel1Menu() {
         this.setLayout(null);
+        // Cargar la imagen solo una vez
+        ImageIcon icon = new ImageIcon(getClass().getResource("/com/ilerna/resources/Menu.png"));
+        imagenFondo = icon.getImage();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        ImageIcon icon = new ImageIcon(getClass().getResource("/com/ilerna/resources/Menu.png"));
-        g.drawImage(icon.getImage(), 0, 0, getWidth(), getHeight(), this);
+        g.drawImage(imagenFondo, 0, 0, getWidth(), getHeight(), this);
     }
 }
