2dbe861dade3214553ef9cad6b69fc5faf816abe
dariorumi5 <dariorumi5@alumnos.ilerna.com>
2026-03-05 13:55:31 +0100

    Clase nave creada

--- khaenriah/src/main/java/com/ilerna/modelos/Nave.java
+++ khaenriah/src/main/java/com/ilerna/modelos/Nave.java
@@ -1,5 +1,25 @@
 package com.ilerna.modelos;
 
-public class Nave {
+import java.awt.Rectangle;
 
+public class Nave {
+    public int x;
+    public int y;
+    public int velocidad;
+    public int vida;
+    public int ancho;
+    public int alto;
+
+    public Nave(int x, int y, int velocidad, int vida) {
+        this.x = x;
+        this.y = y;
+        this.velocidad = velocidad;
+        this.vida = vida;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, ancho, alto);
+    }
 }
