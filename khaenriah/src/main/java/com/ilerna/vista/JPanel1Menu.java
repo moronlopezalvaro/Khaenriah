a9d1bf2accfa10dd67db558cd3efd8b93ba075c9
dariorumi5 <dariorumi5@alumnos.ilerna.com>
2026-03-05 13:57:05 +0100

    Clase JPanel1Menu hecha

--- khaenriah/src/main/java/com/ilerna/vista/JPanel1Menu.java
+++ khaenriah/src/main/java/com/ilerna/vista/JPanel1Menu.java
@@ -1,5 +1,20 @@
 package com.ilerna.vista;
 
-public class JPanel1Menu {
+import java.awt.*;
 
+import javax.swing.*;
+
+public class JPanel1Menu extends JPanel {
+
+    public JPanel1Menu() {
+        this.setLayout(null);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Dimension dimension = getSize();
+        ImageIcon icon = new ImageIcon(getClass().getResource("/resources/Menu.png"));
+        g.drawImage(icon.getImage(), 0, 0, dimension.width, dimension.height, this);
+    }
 }
