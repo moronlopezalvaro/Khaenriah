b5b143bf87961c64c650f167d4f3a6a5e6f6262c
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-03-05 17:58:47 +0100

    clase jefe hecha

--- khaenriah/src/main/java/com/ilerna/modelos/Jefe.java
+++ khaenriah/src/main/java/com/ilerna/modelos/Jefe.java
@@ -1,5 +1,25 @@
 package com.ilerna.modelos;
 
-public class Jefe {
+import java.awt.Rectangle;
 
+public class Jefe {
+    public int x;
+    public int y;
+    public int velocidad = 8;
+    public int vida;
+    public int ancho;
+    public int alto;
+
+    public Jefe(int x, int y, int velocidad, int vida) {
+        this.x = x;
+        this.y = y;
+        this.velocidad = velocidad;
+        this.vida = vida;
+        this.ancho = 100;
+        this.alto = 100;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, ancho, alto);
+    }
 }
