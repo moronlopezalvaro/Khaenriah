9742eaf6a6bd60ca900162b2485c4acb8de64ac0
dariorumi5 <dariorumi5@alumnos.ilerna.com>
2026-03-05 13:57:52 +0100

    Clase VentanaDialogo hecha

--- khaenriah/src/main/java/com/ilerna/vista/VentanaDialogo.java
+++ khaenriah/src/main/java/com/ilerna/vista/VentanaDialogo.java
@@ -1,5 +1,17 @@
 package com.ilerna.vista;
 
-public class VentanaDialogo {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class VentanaDialogo extends JFrame {
+    public VentanaDialogo() {
+        this.setTitle("NOMBRE");
+        this.setSize(new Dimension(1080, 720));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
 
 }
