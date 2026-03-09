742befe01a6901f7810ac3a8856e5b1b278b576a
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-03-09 10:55:40 +0100

    boton siguiente arreglado

--- khaenriah/src/main/java/com/ilerna/vista/JPanelDialogo.java
+++ khaenriah/src/main/java/com/ilerna/vista/JPanelDialogo.java
@@ -30,7 +30,7 @@ public class JPanelDialogo extends JPanel {
         textoDialogo.setWrapStyleWord(true);
 
         // Posicionarlo en el panel (ajusta estos números luego)
-        textoDialogo.setBounds(250, 530, 600, 100);
+        textoDialogo.setBounds(250, 530, 400, 100);
 
         this.add(textoDialogo);
     }
