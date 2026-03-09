576169836cab59d9f72fda2d265404f755f77883
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-03-09 09:58:34 +0100

    texto dialogo añadido

--- khaenriah/src/main/java/com/ilerna/vista/JPanelDialogo.java
+++ khaenriah/src/main/java/com/ilerna/vista/JPanelDialogo.java
@@ -6,12 +6,37 @@ import javax.swing.*;
 public class JPanelDialogo extends JPanel {
 
     private Image imagenFondo;
+    private JTextArea textoDialogo;
 
     public JPanelDialogo() {
         this.setLayout(null);
         // Cargar la imagen solo una vez
-        ImageIcon icon = new ImageIcon(getClass().getResource("/com/ilerna/resources/planeta.jpg"));
+        ImageIcon icon = new ImageIcon(getClass().getResource("/com/ilerna/resources/dialogocoloson.jpg"));
         imagenFondo = icon.getImage();
+
+        // Inicializar el área de texto
+        textoDialogo = new JTextArea();
+        textoDialogo.setText("¡Hola! Soy Coloson. Bienvenido a Khaenriah. \nAquí es donde empieza tu aventura.");
+        textoDialogo.setFont(new Font("Arial", Font.BOLD, 18));
+        textoDialogo.setForeground(Color.WHITE);
+
+        // Hacerlo transparente y de solo lectura
+        textoDialogo.setOpaque(false);
+        textoDialogo.setEditable(false);
+        textoDialogo.setFocusable(false);
+
+        // Saltos de línea automáticos
+        textoDialogo.setLineWrap(true);
+        textoDialogo.setWrapStyleWord(true);
+
+        // Posicionarlo en el panel (ajusta estos números luego)
+        textoDialogo.setBounds(50, 400, 700, 100);
+
+        this.add(textoDialogo);
+    }
+
+    public void setTexto(String nuevoTexto) {
+        textoDialogo.setText(nuevoTexto);
     }
 
     @Override
